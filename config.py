fd5198516be18c8c6ee220c51e7a3427e5dfa148
teddy12391 <sonubgmi0@gmail.com>
2024-07-31 12:55:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY Krishna
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6416410825:AAGDgCan_DEexdlECMY7azL5hbMri4350BM")
 
-  #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+  #Enter Your telegram username here without @Harekrishnaharehareram
+OWNER_USERNAME=("Harekrishnaharehareram")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6914567930")
 
 
 
